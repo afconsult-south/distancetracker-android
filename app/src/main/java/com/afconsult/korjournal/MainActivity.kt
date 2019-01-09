6b9c9c4f19ebb137b0279b323768e0dd4fcd520d
Adrian Landborn <adrian.c.nae@gmail.com>
2019-01-09 13:31:25 +0100

    Cleanup

--- app/src/main/java/com/afconsult/korjournal/MainActivity.kt
+++ app/src/main/java/com/afconsult/korjournal/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.afconsult.korjournal
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.location.Location
-import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -62,14 +54,10 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun openMaps() {
-        startActivity(Intent(this, MapsActivity::class.java))
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_map -> {
-                openMaps()
+            R.id.action_settings -> {
+//                openSettings()
                 true
             }
             else -> super.onOptionsItemSelected(item)
